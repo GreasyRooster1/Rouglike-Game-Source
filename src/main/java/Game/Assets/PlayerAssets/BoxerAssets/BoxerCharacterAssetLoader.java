ea4204d766e0663ac9f983ccffb09c2077368368
QuantumDeity <greasyRooster1@gmail.com>
2023-09-20 19:21:14 -0700

    Tried fixing boxer attack

---
+++ src/main/java/Game/Assets/PlayerAssets/BoxerAssets/BoxerCharacterAssetLoader.java
@@ -0,0 +1,22 @@
+package Game.Assets.PlayerAssets.BoxerAssets;
+
+import System.Setup.Setup;
+import System.TextureLoader.TextureLoader;
+import processing.core.PApplet;
+
+import static System.Util.CreateAnimation.createAnimation;
+
+public class BoxerCharacterAssetLoader {
+    public static void loadAssets(){
+        PApplet a = Setup.getApplet();
+        TextureLoader.registerTexture("Assets\\Player\\BoxerCharacter\\Right.png","player.boxerCharacter.right");
+        TextureLoader.registerTexture("Assets\\Player\\BoxerCharacter\\Left.png","player.boxerCharacter.left");
+        TextureLoader.registerTexture("Assets\\Player\\BoxerCharacter\\IdleRight.png","player.boxerCharacter.idleRight");
+        TextureLoader.registerTexture("Assets\\Player\\BoxerCharacter\\IdleLeft.png","player.boxerCharacter.idleLeft");
+        TextureLoader.registerTexture("Assets\\Player\\BoxerCharacter\\AttackParticle.png","player.boxerCharacter.attackParticle");
+        TextureLoader.registerTexture("Assets\\Player\\BoxerCharacter\\BoxingGlove.png","player.boxerCharacter.boxingGlove");
+        createAnimation("Assets\\Player\\BoxerCharacter\\LeftWalk","player.boxerCharacter.leftWalk",5,3);
+        createAnimation("Assets\\Player\\BoxerCharacter\\RightWalk","player.boxerCharacter.rightWalk",5,3);
+        createAnimation("Assets\\Player\\BoxerCharacter\\AttackAnim","player.boxerCharacter.attack",4,4);
+    }
+}
