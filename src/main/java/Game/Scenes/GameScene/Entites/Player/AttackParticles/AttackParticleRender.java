2b21e021d1d52bb7349b134423f3e3999b4521c3
Quantum <78569560+GreasyRooster1@users.noreply.github.com>
2023-09-21 02:32:37 +0000

    idk what im doing

---
+++ src/main/java/Game/Scenes/GameScene/Entites/Player/AttackParticles/AttackParticleRender.java
@@ -0,0 +1,9 @@
+package Game.Scenes.GameScene.Entites.Player.AttackParticles;
+
+import System.Entity.BaseEntity.Renders.EntityImageRender;
+
+public class AttackParticleRender extends EntityImageRender {
+    AttackParticleRender(){
+        texturePath = "other.missing";
+    }
+}
--- src/main/java/Game/Scenes/GameScene/Entites/Player/CharacterTypes/Boxer/Boxer.java
+++ src/main/java/Game/Scenes/GameScene/Entites/Player/CharacterTypes/Boxer/Boxer.java
@@ -1,18 +1,15 @@
 package Game.Scenes.GameScene.Entites.Player.CharacterTypes.Boxer;
 
 import Game.Scenes.GameScene.Entites.Player.Player;
-import Game.Scenes.GameScene.Entites.Player.Upgrades.BasicUpgrades.AttackUpgrade;
-import Game.Scenes.GameScene.Entites.Player.Upgrades.BasicUpgrades.DefenceUpgrade;
-import Game.Scenes.GameScene.Entites.Player.Upgrades.BasicUpgrades.SpeedUpgrade;
-import System.Logging.Logger;
 
 public class Boxer extends Player {
     public Boxer(float xa, float ya) {
         super(xa, ya);
         setAttackType("melee");
-        setTextureName("basicCharacter");
+        setTextureName("boxerCharacter");
+        setAttackAnimation("attackParticle");
         name="Boxer";
-        ATK_SPEED = 10;
+        ATK_SPEED = 20;
         KB=20;
     }
     public void setUpgrades(){
