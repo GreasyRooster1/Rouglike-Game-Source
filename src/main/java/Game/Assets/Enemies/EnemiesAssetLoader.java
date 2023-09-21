e37ccf457872d9b1d09b0ed1959509b05f364761
Quantum <78569560+GreasyRooster1@users.noreply.github.com>
2023-09-21 02:34:47 +0000

    deleted

--- src/main/java/Game/Assets/Enemies/EnemiesAssetLoader.java
+++
@@ -1,9 +0,0 @@
-package Game.Assets.Enemies;
-
-import Game.Assets.Enemies.Slimes.SlimesAssetLoader;
-
-public class EnemiesAssetLoader {
-    public static void loadAssets(){
-        SlimesAssetLoader.loadAssets();
-    }
-}
--- src/main/java/Game/Scenes/GameScene/Entites/Player/AttackParticles/AttackParticleRender.java
+++
@@ -1,9 +0,0 @@
-package Game.Scenes.GameScene.Entites.Player.AttackParticles;
-
-import System.Entity.BaseEntity.Renders.EntityImageRender;
-
-public class AttackParticleRender extends EntityImageRender {
-    AttackParticleRender(){
-        texturePath = "other.missing";
-    }
-}
